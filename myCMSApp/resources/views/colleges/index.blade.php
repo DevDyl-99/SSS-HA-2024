5fa75fb32aa88b7ca123fb445caab897b7053920
DevDyl-99 <devdyl.webagency@gmail.com>
2025-02-26 12:12:24 +0000

    Created a master layout file and integrated the index college index file with it

--- myCMSApp/resources/views/colleges/index.blade.php
+++ myCMSApp/resources/views/colleges/index.blade.php
@@ -1,39 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Colleges List</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css">
-</head>
-<body>
+@extends('layouts.app')
 
-<div class="container mt-5">
-    <h1>Colleges</h1>
-    <!-- Check if there are any colleges -->
-    @if($colleges->count() > 0)
-        <table class="table table-bordered">
-            <thead>
+@section('content')
+    <div class="d-flex justify-content-between mb-3">
+        <h2>Colleges</h2>
+    </div>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Address</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($colleges as $college)
                 <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Address</th>
+                    <td>{{ $college->id }}</td>
+                    <td>{{ $college->name }}</td>
+                    <td>{{ $college->address }}</td>
                 </tr>
-            </thead>
-            <tbody>
-                @foreach($colleges as $college)
-                    <tr>
-                        <td>{{ $college->id }}</td>
-                        <td>{{ $college->name }}</td>
-                        <td>{{ $college->address }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @else
-        <p>No colleges found.</p>
-    @endif
-</div>
-
-</body>
-</html>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
